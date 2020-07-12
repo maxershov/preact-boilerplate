e8faaa1d1aebc227701b28fefe259b6b5b441f9b
Max Ershov <maxx95.95@gmail.com>
2020-07-12 12:41:40 +0300

    Add postCSS and prod webpack config

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: {
+        "cssnano": { preset: 'default' },
+        "postcss-preset-env": {
+            stage: 1,
+            autoprefixer: { grid: true },
+            browsers: [">1%", "not OperaMini all"]
+        }
+    }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,18 @@
+const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname),
-  mode: "development",
   entry: ["@babel/polyfill", "./src/index.js"],
-  devtool: "source-map",
+  mode: "production",
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "main.js",
-    chunkFilename: "[name].bundle.js",
+    filename: "[name].bundle.js",
     publicPath: "/"
-  },
-  devServer: {
-    port: 6801,
-    open: true,
-    hot: true,
-    watchContentBase: true,
-    progress: true,
-    contentBase: path.join(__dirname, "dist"),
-    writeToDisk: true,
-    overlay: true,
-    historyApiFallback: true // on 404 load publicPath => for BrowserRouter on refresh
   },
   resolve: {
     extensions: [".jsx", ".js", ".json"],
@@ -53,13 +43,23 @@ module.exports = {
             }
           }
         ]
+      }, {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/',
+            esModule: false
+          }
+        }]
       },
       {
         loader: require.resolve("file-loader"),
-        exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/, /\.css$/],
+        exclude: [/\.(js|mjs|jsx|ts|tsx|jpe?g|png|gif|svg)$/, /\.html$/, /\.json$/, /\.css$/],
         options: {
           name: "static/media/[name].[hash:8].[ext]",
-          esModule: false // fix problem with img [object Module] in browser
+          esModule: false // fix problem with img [object Module]
         }
       }
     ]
@@ -68,6 +68,33 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "assets", "index.html"),
       title: "ChangeME",
+      favicon: path.join(__dirname, "src", "assets", "favicon.ico"),
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        keepClosingSlash: true,
+        minifyURLs: true
+      }
+    }),
+    new CompressionPlugin({
+      algorithm: "gzip"
+    }),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          unsafe: true,
+          inline: true,
+          passes: 2,
+          keep_fargs: false
+        },
+        output: {
+          beautify: false,
+        },
+      },
+      parallel: true
     }),
     new MiniCssExtractPlugin()
   ]
