c3893579d892e029d02c6027c57485a69fb2e1d3
Max Ershov <maxx95.95@gmail.com>
2020-10-31 18:57:30 +0300

    feat: add preact dev-tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'preact/compat';
 import './App.css';
-
 import Classes from './Components/Classes';
 import Hooks from './Components/Hooks';
+
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line global-require
+  require("preact/debug");
+}
+
 
 const App = (props) => {
   return (
     <>
-      <Classes />
       <Hooks />
     </>
   );
